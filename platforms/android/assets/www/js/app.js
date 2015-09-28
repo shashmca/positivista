d49a49b797189497db5d0d5838b1dc74588f939a
Shashank Shrivastava <pvijan@sapient.com>
2015-09-28 23:20:53 +0530

    updated

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -26,105 +26,161 @@ angular.module('positivista', ['ionic', 'positivista.controllers'])
             }
         });
     })
-.config(function($ionicConfigProvider) {
-    $ionicConfigProvider.backButton.text('');
-})
-.config(function($stateProvider, $urlRouterProvider) {
-    $stateProvider
-        .state('app', {
-            url: '/app',
-            abstract: true,
-            templateUrl: 'templates/menu.html',
-            controller: 'AppCtrl'
+    .config(function($ionicConfigProvider) {
+        $ionicConfigProvider.backButton.text('');
+    })
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('app', {
+                url: '/app',
+                abstract: true,
+                templateUrl: 'templates/menu.html',
+                controller: 'AppCtrl'
+            })
+
+        .state('app.playlists', {
+            url: '/playlists',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/playlists.html',
+                    controller: 'PlaylistsCtrl'
+                }
+            }
         })
 
-    .state('app.playlists', {
-        url: '/playlists',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/playlists.html',
-                controller: 'PlaylistsCtrl'
+        .state('app.single', {
+            url: '/playlists/:playlistId',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/playlist.html',
+                    controller: 'PlaylistCtrl'
+                }
             }
-        }
-    })
+        })
 
-    .state('app.single', {
-        url: '/playlists/:playlistId',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/playlist.html',
-                controller: 'PlaylistCtrl'
+        .state('setprofile', {
+            url: '/setprofile',
+            templateUrl: 'templates/setprofile.html',
+            controller: 'SetProfileCtrl',
+        })
+
+        .state('mandgoal', {
+            url: '/mandgoal',
+            templateUrl: 'templates/mandgoal.html',
+            controller: 'MandGoalsCtrl',
+        })
+
+        .state('usergoal', {
+            url: '/usergoal',
+            templateUrl: 'templates/usergoal.html',
+            controller: 'UserGoalsCtrl',
+        })
+
+        .state('setreminder', {
+            url: '/setreminder',
+            templateUrl: 'templates/setreminder.html',
+            controller: 'SetReminderCtrl',
+        })
+
+        .state('thankyou', {
+            url: '/thankyou',
+            templateUrl: 'templates/thankyou.html',
+            controller: 'ThankYouCtrl',
+        })
+
+        .state('register', {
+            url: '/register',
+            templateUrl: 'templates/register.html',
+            controller: 'AuthCtrl',
+            controllerAs: 'authCtrl'
+        })
+
+        .state('forgotpassword', {
+            url: '/forgotpassword',
+            templateUrl: 'templates/forgotpassword.html',
+            controller: 'AuthCtrl',
+            controllerAs: 'authCtrl'
+        })
+
+        .state('app.homepage', {
+            url: '/homepage',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/homepage.html',
+                    controller: 'HomePageCtrl'
+                }
             }
-        }
-    })
+        })
 
-    .state('setprofile', {
-        url: '/setprofile',
-        templateUrl: 'templates/setprofile.html',
-        controller: 'SetProfileCtrl',
-    })
-
-    .state('mandgoal', {
-        url: '/mandgoal',
-        templateUrl: 'templates/mandgoal.html',
-        controller: 'MandGoalsCtrl',
-    })
-
-    .state('usergoal', {
-        url: '/usergoal',
-        templateUrl: 'templates/usergoal.html',
-        controller: 'UserGoalsCtrl',
-    })
-
-    .state('setreminder', {
-        url: '/setreminder',
-        templateUrl: 'templates/setreminder.html',
-        controller: 'SetReminderCtrl',
-    })
-
-    .state('thankyou', {
-        url: '/thankyou',
-        templateUrl: 'templates/thankyou.html',
-        controller: 'ThankYouCtrl',
-    })
-
-    .state('register', {
-        url: '/register',
-        templateUrl: 'templates/register.html',
-        controller: 'AuthCtrl',
-        controllerAs: 'authCtrl'
-    })
-
-    .state('app.homepage', {
-        url: '/homepage',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/homepage.html',
-                controller: 'HomePageCtrl'
+        .state('app.managegoals', {
+            url: '/managegoals',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/managegoals.html',
+                    controller: 'ManageGoalsCtrl'
+                }
             }
-        }
-    })
+        })
 
-    .state('app.managegoals', {
-        url: '/managegoals',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/managegoals.html',
-                controller: 'ManageGoalsCtrl'
+        .state('app.managereport', {
+            url: '/managereport',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/managereport.html',
+                    controller: 'ManageReportCtrl'
+                }
             }
-        }
-    })
+        })
 
-    .state('appinit', {
-        url: '/appinit',
-        templateUrl: function() {
-            return 'templates/login.html';
-        },
-        controller: 'AuthCtrl',
-        controllerAs: 'authCtrl'
-    })
+        .state('app.about', {
+            url: '/about',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/about.html',
+                    controller: 'AboutCtrl'
+                }
+            }
+        })
 
+        .state('app.manageprofile', {
+            url: '/manageprofile',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/manageprofile.html',
+                    controller: 'ManageProfileCtrl'
+                }
+            }
+        })
 
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/appinit');
-});
+        .state('app.managereminders', {
+            url: '/managereminders',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/managereminders.html',
+                    controller: 'ManageRemindersCtrl'
+                }
+            }
+        })
+
+        .state('app.settings', {
+            url: '/settings',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/settings.html',
+                    controller: 'SettingsCtrl'
+                }
+            }
+        })
+
+        .state('appinit', {
+            url: '/appinit',
+            templateUrl: function() {
+                return 'templates/login.html';
+            },
+            controller: 'AuthCtrl',
+            controllerAs: 'authCtrl'
+        })
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/appinit');
+    });
--- platforms/android/assets/www/js/controllers.js
+++ platforms/android/assets/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('positivista.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', ['$scope', '$ionicModal', '$timeout', '$location', 'SessionService', function($scope, $ionicModal, $timeout, $location, SessionService) {
 
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
@@ -8,43 +8,32 @@ angular.module('positivista.controllers', [])
     // listen for the $ionicView.enter event:
     //$scope.$on('$ionicView.enter', function(e) {
     //});
+    if (SessionService.isLoggedInUser()) {
+        $scope.doLogout = function() {
+            if (confirm("Are you sure to logout?")) {
+                localStorage.clear();
+                $location.url("/appinit");
+            }
+        }
+    } else {
+        $location.url("/appinit");
+    }
 
-})
 
-.controller('PlaylistsCtrl', function($scope) {
-    $scope.playlists = [{
-        title: 'Reggae',
-        id: 1
-    }, {
-        title: 'Chill',
-        id: 2
-    }, {
-        title: 'Dubstep',
-        id: 3
-    }, {
-        title: 'Indie',
-        id: 4
-    }, {
-        title: 'Rap',
-        id: 5
-    }, {
-        title: 'Cowbell',
-        id: 6
-    }];
-})
+}])
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {})
 
-.controller('HomePageCtrl', ['$scope', '$location', '$stateParams',function($scope, $location, $stateParams) {
+.controller('HomePageCtrl', ['$scope', '$location', '$stateParams', function($scope, $location, $stateParams) {
     localStorage.setItem('currPage', 'app/homepage');
 
     $scope.goToMangeGoals = function() {
         $location.url('/app/managegoals');
     }
+
 }])
 
-.controller('ManageGoalsCtrl', function($scope, $stateParams) {
-    $scope.goalList = [{
+.controller('ManageGoalsCtrl', ['$scope', function($scope, $stateParams) {
+    var goalList = [{
         "title": "Set clear goals?"
     }, {
         "title": "Make progress towards goal achievements?"
@@ -57,130 +46,322 @@ angular.module('positivista.controllers', [])
     }, {
         "title": "Be fully engaged?"
     }];
-})
 
-.controller('SetProfileCtrl', ['GetUserInfo','ConvertImage', 'AlertService', 'UserService', '$location', '$scope', '$ionicPopup', function(GetUserInfo, ConvertImage, AlertService, UserService, $location, $scope, $ionicPopup) {
-    localStorage.setItem('currPage', 'setprofile');
+    $scope.groups = [];
+    for (var i = 0; i < goalList.length; i++) {
+        $scope.groups[i] = {
+            name: goalList[i].title,
+            items: []
+        };
+        for (var j = 0; j < 1; j++) {
+            $scope.groups[i].items.push(i + '-' + j);
+        }
+    }
 
-    var userId = GetUserInfo.getUserId();
-    GetUserInfo.getProfileData().then(function(data) {
-        if(data) {
-            data = JSON.parse(data);
-            $scope.profileName = data.profileName;
-            $scope.profileStatus = data.profileStatus;
-            $scope.profileImg = data.profileImg;
+    /*
+     * if given group is the selected group, deselect it
+     * else, select the given group
+     */
+    $scope.toggleGroup = function(group) {
+        if ($scope.isGroupShown(group)) {
+            $scope.shownGroup = null;
         } else {
-            $scope.profileName = "";
-            $scope.profileStatus = "";
-            $scope.profileImg = "";
+            $scope.shownGroup = group;
         }
-    },
-    function(err) {
-        $scope.profileName = "";
-        $scope.profileStatus = "";
-        $scope.profileImg = "";
-    })
-    
+    };
+    $scope.isGroupShown = function(group) {
+        return $scope.shownGroup === group;
+    };
+}])
 
-    
-    $scope.saveProfileName = function() {
-        if (this.profileName) {
-            localStorage.setItem("profileName", this.profileName);
+.controller('SetProfileCtrl', ['GetUserInfo', 'ConvertImage', 'AlertService', 'UserService', '$location', '$scope', '$ionicPopup', 'SessionService', function(GetUserInfo, ConvertImage, AlertService, UserService, $location, $scope, $ionicPopup, SessionService) {
+    if (SessionService.isLoggedInUser()) {
+        localStorage.setItem('currPage', 'setprofile');
+
+        var userId = localStorage.getItem('userId');
+        if (!userId) {
+            userId = GetUserInfo.getUserId();
         }
-        UserService.updateProfileName(this.profilename, userId).then(function(user) {
-            console.log("Profile name saved in DB");
-        }, function(err) {
-            console.log("Profile name not saved in DB");
-        });
+
+
+        if (localStorage.getItem('profileName') && localStorage.getItem('profileStatus') && localStorage.getItem('profileImg')) {
+            $scope.profileName = localStorage.getItem('profileName');
+            $scope.profileStatus = localStorage.getItem('profileStatus');
+            $scope.imgSrc = localStorage.getItem('profileImg');
+        } else {
+            GetUserInfo.getProfileData().then(function(data) {
+                    if (data != "null") {
+                        data = JSON.parse(data);
+                        $scope.profileName = data.profileName;
+                        $scope.profileStatus = data.profileStatus;
+                        if (data.profileImg) {
+                            $scope.imgSrc = data.profileImg;
+                        } else {
+                            $scope.imgSrc = "../../cat.png";
+                        }
+                    } else {
+                        $scope.profileName = "";
+                        $scope.profileStatus = "";
+                        $scope.imgSrc = "../../cat.png";
+                    }
+                },
+                function(err) {
+                    $scope.profileName = "";
+                    $scope.profileStatus = "";
+                    $scope.profileImg = "";
+                })
+        }
+
+        $scope.saveProfileData = function() {
+            if (this.profileName) {
+                localStorage.setItem("profileName", this.profileName);
+            }
+            if (this.profileStatus) {
+                localStorage.setItem("profileStatus", this.profileStatus);
+            }
+
+            UserService.updateProfile(this.profileName, this.profileStatus, userId).then(function(user) {
+                console.log("Profile updated successfully");
+            }, function(err) {
+                console.log("Profile update failed");
+            });
+
+        }
+
+        $scope.saveProfileImage = function(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function(e) {
+                    var imgPath = e.target.result;
+                    localStorage.setItem("profileImg", imgPath);
+                    $scope.imgSrc = imgPath;
+                    $scope.$apply();
+                    UserService.updateProfileImg(imgPath, userId).then(function(user) {
+                        console.log("Profile image updated successfully");
+                    }, function(err) {
+                        console.log("Profile image update failed");
+                    });
+                }
+
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+    } else {
+        $location.url("/appinit");
     }
-
-    $scope.saveProfileStatus = function() {
-        console.log("Status  ", this.profileStatus)
-        localStorage.setItem("profileStatus", this.profileStatus);
-        UserService.updateProfileStatus(this.profileStatus, userId).then(function(user) {
-            console.log("Profile status saved in DB");
-        }, function(err) {
-            console.log("Profile status not saved in DB");
-        });
-    }
-
-    $scope.saveProfileImage = function(elem) {
-        console.log("=====",elem)
-        ConvertImage.Base64(elem.value, function(base64Img) {
-            console.log(base64Img)
-        });
-        /*localStorage.setItem("profileImg ", this.profileImg);
-        UserService.updateProfileStatus(this.profileImg, userId).then(function(user) {
-            console.log("Profile image saved in DB");
-        }, function(err) {
-            console.log("Profile image not saved in DB");
-        });*/
-    }
-
-
-
 
 }])
 
-.controller('MandGoalsCtrl', function($scope, $stateParams) {
-    localStorage.setItem('currPage', 'mandgoal');
-    $scope.goalList = [{
-        "title": "Set clear goals?"
-    }, {
-        "title": "Make progress towards goal achievements?"
-    }, {
-        "title": "Find meaning?"
-    }, {
-        "title": "Be happy?"
-    }, {
-        "title": "Build positive relationships?"
-    }, {
-        "title": "Be fully engaged?"
-    }];
+.controller('MandGoalsCtrl', ['$location', '$scope', '$stateParams', 'GoalService', 'SessionService', function($location, $scope, $stateParams, GoalService, SessionService) {
+    if (SessionService.isLoggedInUser()) {
+        localStorage.setItem('currPage', 'mandgoal');
+        GoalService.fetchMandatoryGoals().then(function(succObj) {
+                $scope.goalList = JSON.parse(succObj.data.data);
+                $scope.checkbox = [];
+                for (var i = 0; i < $scope.goalList.length; i++) {
+                    $scope.checkbox[i] = false;
+                }
+            },
+            function(err) {
 
-    $scope.checkbox = [];
-    for (var i = 0; i < $scope.goalList.length; i++) {
-        $scope.checkbox[i] = false;
+            })
+
+
+        /*[{
+            "title": "Set clear goals?"
+        }, {
+            "title": "Make progress towards goal achievements?"
+        }, {
+            "title": "Find meaning?"
+        }, {
+            "title": "Be happy?"
+        }, {
+            "title": "Build positive relationships?"
+        }, {
+            "title": "Be fully engaged?"
+        }];*/
+
+
+
+        $scope.isChecked = function(index) {
+            $scope.checkbox[index] = !$scope.checkbox[index];
+        }
+    } else {
+        $location.url("/appinit");
     }
 
-    $scope.isChecked = function(index) {
-        $scope.checkbox[index] = !$scope.checkbox[index];
+}])
+
+.controller('UserGoalsCtrl', ['SessionService', '$location', '$scope', function(SessionService, $location, $scope, $stateParams) {
+    if (SessionService.isLoggedInUser()) {
+        localStorage.setItem('currPage', 'usergoal');
+        $scope.goalList = [{
+            "title": "Set clear goals?"
+        }, {
+            "title": "Make progress towards goal achievements?"
+        }, {
+            "title": "Find meaning?"
+        }, {
+            "title": "Be happy?"
+        }, {
+            "title": "Build positive relationships?"
+        }, {
+            "title": "Be fully engaged?"
+        }]
+
+        $scope.checkbox = [];
+        for (var i = 0; i < $scope.goalList.length; i++) {
+            $scope.checkbox[i] = false;
+        }
+        $scope.isChecked = function(index) {
+            $scope.checkbox[index] = !$scope.checkbox[index];
+        }
+    } else {
+        $location.url("/appinit");
+    }
+}])
+
+.controller('SetReminderCtrl', ['SessionService', '$location', '$scope', function(SessionService, $location, $scope, $stateParams) {
+    if (SessionService.isLoggedInUser()) {
+        localStorage.setItem('currPage', 'setreminder');
+    } else {
+        $location.url("/appinit");
+    }
+}])
+
+.controller('ThankYouCtrl', ['GetUserInfo','SessionService', '$location', '$scope', function(GetUserInfo, SessionService, $location, $scope, $stateParams) {
+    if (SessionService.isLoggedInUser()) {
+        localStorage.setItem('currPage', 'thankyou');
+
+        var userId = localStorage.getItem('userId');
+        if (!userId) {
+            userId = GetUserInfo.getUserId();
+        }
+
+
+        if (localStorage.getItem('profileName') && localStorage.getItem('profileImg')) {
+            $scope.profileName = localStorage.getItem('profileName');
+            $scope.imgSrc = localStorage.getItem('profileImg');
+        } else {
+            GetUserInfo.getProfileData().then(function(data) {
+                    if (data != "null") {
+                        data = JSON.parse(data);
+                        $scope.profileName = data.profileName;
+                        if (data.profileImg) {
+                            $scope.imgSrc = data.profileImg;
+                        } else {
+                            //$scope.imgSrc = "../../cat.png";
+                        }
+                    } else {
+                        $scope.profileName = "";
+                        //$scope.imgSrc = "../../cat.png";
+                    }
+                },
+                function(err) {
+                    $scope.profileName = "";
+                    $scope.profileImg = "";
+                })
+        }
+    } else {
+        $location.url("/appinit");
+    }
+}])
+
+.controller('ManageReportCtrl', ['$location', '$scope', function($location, $scope, $stateParams) {
+
+}])
+
+.controller('AboutCtrl', ['$location', '$scope', function($location, $scope, $stateParams) {
+
+}])
+
+.controller('ManageProfileCtrl', ['GetUserInfo', 'ConvertImage', 'AlertService', 'UserService', '$location', '$scope', '$ionicPopup', 'SessionService', function(GetUserInfo, ConvertImage, AlertService, UserService, $location, $scope, $ionicPopup, SessionService) {
+    if (SessionService.isLoggedInUser()) {
+        localStorage.setItem('currPage', 'setprofile');
+
+        var userId = localStorage.getItem('userId');
+        if (!userId) {
+            userId = GetUserInfo.getUserId();
+        }
+
+
+        if (localStorage.getItem('profileName') && localStorage.getItem('profileStatus') && localStorage.getItem('profileImg')) {
+            $scope.profileName = localStorage.getItem('profileName');
+            $scope.profileStatus = localStorage.getItem('profileStatus');
+            $scope.imgSrc = localStorage.getItem('profileImg');
+        } else {
+            GetUserInfo.getProfileData().then(function(data) {
+                    if (data != "null") {
+                        data = JSON.parse(data);
+                        $scope.profileName = data.profileName;
+                        $scope.profileStatus = data.profileStatus;
+                        if (data.profileImg) {
+                            $scope.imgSrc = data.profileImg;
+                        } else {
+                            $scope.imgSrc = "../../cat.png";
+                        }
+                    } else {
+                        $scope.profileName = "";
+                        $scope.profileStatus = "";
+                        $scope.imgSrc = "../../cat.png";
+                    }
+                },
+                function(err) {
+                    $scope.profileName = "";
+                    $scope.profileStatus = "";
+                    $scope.profileImg = "";
+                })
+        }
+
+        $scope.saveProfileData = function() {
+            if (this.profileName) {
+                localStorage.setItem("profileName", this.profileName);
+            }
+            if (this.profileStatus) {
+                localStorage.setItem("profileStatus", this.profileStatus);
+            }
+
+            UserService.updateProfile(this.profileName, this.profileStatus, userId).then(function(user) {
+                console.log("Profile updated successfully");
+            }, function(err) {
+                console.log("Profile update failed");
+            });
+
+        }
+
+        $scope.saveProfileImage = function(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function(e) {
+                    var imgPath = e.target.result;
+                    localStorage.setItem("profileImg", imgPath);
+                    $scope.imgSrc = imgPath;
+                    $scope.$apply();
+                    UserService.updateProfileImg(imgPath, userId).then(function(user) {
+                        console.log("Profile image updated successfully");
+                    }, function(err) {
+                        console.log("Profile image update failed");
+                    });
+                }
+
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+    } else {
+        $location.url("/appinit");
     }
 
-})
+}])
 
-.controller('UserGoalsCtrl', function($scope, $stateParams) {
-    localStorage.setItem('currPage', 'usergoal');
-    $scope.goalList = [{
-        "title": "Set clear goals?"
-    }, {
-        "title": "Make progress towards goal achievements?"
-    }, {
-        "title": "Find meaning?"
-    }, {
-        "title": "Be happy?"
-    }, {
-        "title": "Build positive relationships?"
-    }, {
-        "title": "Be fully engaged?"
-    }]
+.controller('ManageRemindersCtrl', ['$location', '$scope', function($location, $scope, $stateParams) {
 
-    $scope.checkbox = [];
-    for (var i = 0; i < $scope.goalList.length; i++) {
-        $scope.checkbox[i] = false;
-    }
-    $scope.isChecked = function(index) {
-        $scope.checkbox[index] = !$scope.checkbox[index];
-    }
-})
+}])
 
-.controller('SetReminderCtrl', function($scope, $stateParams) {
-    localStorage.setItem('currPage', 'setreminder');
-})
+.controller('SettingsCtrl', ['$location', '$scope', function($location, $scope, $stateParams) {
 
-.controller('ThankYouCtrl', function($scope, $stateParams) {
-    localStorage.setItem('currPage', 'thankyou');
-})
+}])
 
 .controller('AuthCtrl', ['AlertService', 'UserService', '$location', '$scope', '$ionicPopup', function(AlertService, UserService, $location, $scope, $ionicPopup) {
     var self = $scope;
@@ -208,7 +389,15 @@ angular.module('positivista.controllers', [])
     };
     self.doRegister = function() {
         UserService.register(this.username, this.password, this.email).then(function(user) {
-            $location.path('/setprofile');
+            user = JSON.parse(user);
+            if (user.userId != null) {
+                localStorage.setItem('isLoggedIn', true);
+                localStorage.setItem('userId', user.userId);
+                $location.url('/setprofile');
+            } else {
+                alert("Registration failed!");
+            }
+
         }, function(err) {
             AlertService.set(err.data.msg);
         });
@@ -216,8 +405,17 @@ angular.module('positivista.controllers', [])
     self.goToRegister = function() {
         $location.url("/register");
     };
-    self.reset = function() {
-        this.registerForm = angular.copy(this.master);
+    self.goToLogin = function() {
+        $location.url("/appinit");
+    }
+
+    self.forgotPassword = function() {
+        UserService.fgtpassword(this.email).then(function(data) {
+                alert(data.data.status_message);
+            },
+            function(err) {
+                alert(data.data.status_message);
+            });
     }
 
 }])
